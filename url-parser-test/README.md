4a01225999b89c8ce4fd140b2c303d9a5b17fffb
gerarovetta <139300107+gerarovetta@users.noreply.github.com>
2023-09-27 22:26:08 -0300

    Add files via upload

---
+++ url-parser-test/README.md
@@ -0,0 +1,50 @@
+# URL Format String Parser (Without Lodash)
+
+This JavaScript utility function extracts variable parts of a URL format string and maps them to their values in a hash based on a given URL instance. 
+
+## Project Structure :rocket:
+
+Solution can be found under parser.js file [ parseUrl ]
+
+url-parser-test/
+│
+├── parser.js # JavaScript file containing the URL format string parser function
+│
+├── index.js # JavaScript file main file that make use of urlParse function 
+│
+├── test/
+│ └── test.js # test file with test cases for the parser
+│
+├── package.json # Node.js project configuration
+│
+└── README.md # This README file
+
+
+## Tests Usage
+
+* npm install
+* npm test
+
+## Function Explanation :rocket:
+
+The urlParser function takes two arguments:
+
+    urlFormatString: A string that describes the format of a URL. It can contain constant parts and variable parts, where variable parts start with a colon (:).
+
+    urlInstance: A URL instance that matches the format specified in urlFormatString. It may also contain query parameters.
+
+The function returns an object with the variable parts as keys and their corresponding values from the URL instance.
+
+## Example
+
+Given the example URL format string and URL instance:
+
+```javascript
+const urlFormatString = '/:version/api/:collection/:id';
+const urlInstance = '/6/api/listings/3?sort=desc&limit=10';
+
+const result = urlParser(urlFormatString, urlInstance);
+console.log(result);
+```
+
+
---
+++ url-parser-test/parser.js
@@ -0,0 +1,37 @@
+/**
+ * * Make the animal speak by adding its sound to the input.
+* @param {string} input - A string that describes the format of a URL
+* @param {string} input - A URL instance that matches the format specified in urlFormatString
+* @returns {} 
+*/
+function urlParser(urlFormatString, urlInstance) {
+    // Parse the URL format string to extract variable names.
+    const variableNames = [];
+    const formatParts = urlFormatString.split('/');
+    const urlParts = urlInstance.split('?')[0].split('/'); // we do not need the query string here
+    const variableMap = {}; // return object
+    
+    for (let i = 0; i < formatParts.length; i++) {
+        const formatPart = formatParts[i];
+        const urlPart = urlParts[i];
+    
+        if (formatPart.startsWith(':')) {
+          // This is a variable so we extract the variable name 
+          // from the format and value from url instance.
+          const variableName = formatPart.slice(1);
+          variableMap[variableName] = urlPart;
+        }
+      }
+     // Now we can parse query parameters from the url instance and make use of URLSearchParams
+    const queryString= urlInstance.split('?')[1]; // obtain query information
+    if (queryString) { 
+        const queryParams = new URLSearchParams(queryString);
+        queryParams.forEach((value, key) => {
+        variableMap[key] = value;
+        });
+    }
+    console.log(variableMap);
+    return variableMap;
+}
+
+export default urlParser;
